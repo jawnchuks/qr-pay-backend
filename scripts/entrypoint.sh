4b6ed0629b4e50113c45ccd83eb96b0928ea28cb
jawnchuks <jawnchuks@gmail.com>
2026-03-26 11:52:33 +0100

    feat(database): add seeding to startup script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,5 +8,8 @@ echo "Listing files: $(ls -R)"
 echo "Running database migrations..."
 npx prisma migrate deploy || { echo "Migration failed!"; exit 1; }
 
+echo "Seeding database..."
+npx prisma db seed || { echo "Seeding failed!"; }
+
 echo "Starting application..."
 exec node dist/main.js
